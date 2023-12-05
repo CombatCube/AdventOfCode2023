f42661dae1fa1d6423d84101f990d4aef85b4568
acoccimiglio <acoccimiglio@riotgames.com>
2023-12-05 14:33:08 -0800

    Day 5 part 2 (brute force)

--- day5/day5.py
+++ day5/day5.py
@@ -8,6 +8,8 @@ def parse_seeds(s: str):
     return re.findall(r"\d+", s)
     pass
 
+def parse_seeds_2(s: str):
+    return [(int(match.group(1)), int(match.group(2))) for match in re.finditer(r"(\d+) (\d+)", s)]
 
 class Map:
     def __init__(self, s:str):
@@ -29,12 +31,14 @@ class Map:
         return result if result is not None else num
 
 
+
+
 def main():
     with open("puzzle5") as file:
         locations = []
         sections = file.read().split("\n\n")
         # seeds = parse_seeds(sections[0])
-        seeds = parse_seeds(sections[0])
+        seeds = parse_seeds_2(sections[0])
         seed_soil = Map(sections[1])
         soil_fert = Map(sections[2])
         fert_water = Map(sections[3])
@@ -42,15 +46,16 @@ def main():
         light_temp = Map(sections[5])
         temp_humid = Map(sections[6])
         humid_location = Map(sections[7])
-        for seed in seeds:
-            soil = seed_soil.lookup(int(seed))
-            fert = soil_fert.lookup(int(soil))
-            water = fert_water.lookup(fert)
-            light = water_light.lookup(water)
-            temp = light_temp.lookup(light)
-            humid = temp_humid.lookup(temp)
-            location = humid_location.lookup(humid)
-            locations.append(location)
+        for seed_range in seeds:
+            for seed in range(seed_range[0], seed_range[0] + seed_range[1]):
+                soil = seed_soil.lookup(int(seed))
+                fert = soil_fert.lookup(int(soil))
+                water = fert_water.lookup(fert)
+                light = water_light.lookup(water)
+                temp = light_temp.lookup(light)
+                humid = temp_humid.lookup(temp)
+                location = humid_location.lookup(humid)
+                locations.append(location)
         print(min(locations))
 
 
