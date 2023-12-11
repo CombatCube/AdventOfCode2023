37f3b64818b8a29a9e72333f31197fba1d96c4ee
acoccimi <acoccimi@gmail.com>
2023-12-10 23:43:39 -0800

    Day 11 solution part 2

--- day11/day11.py
+++ day11/day11.py
@@ -6,15 +6,29 @@ def main():
     total = 0
     with open("puzzle11") as file:
         start_array = numpy.asarray([[1 if c == '#' else 0 for c in (list(s.strip()))] for s in file.readlines()])
-        empty_rows = list(numpy.nonzero(numpy.logical_not(numpy.any(start_array, 0))))[0]
-        empty_cols = list(numpy.nonzero(numpy.logical_not(numpy.any(start_array, 1))))[0]
-        array = numpy.insert(start_array, obj=empty_rows, values=False, axis=1)
-        array = numpy.insert(array, obj=empty_cols, values=False, axis=0)
-        coords = numpy.argwhere(array == 1)
-        for i in range(len(coords)):
-            for j in range(i, len(coords)):
-                if i != j:
-                    total += (abs(coords[j][1] - coords[i][1]) + abs(coords[j][0] - coords[i][0]))
+        empty_rows = list(numpy.nonzero(numpy.logical_not(numpy.any(start_array, 1))))[0]
+        empty_cols = list(numpy.nonzero(numpy.logical_not(numpy.any(start_array, 0))))[0]
+        coords = numpy.argwhere(start_array == 1)
+        for a in range(len(coords)):
+            for b in range(a, len(coords)):
+                if a != b:
+                    a_row = coords[a][0]
+                    a_col = coords[a][1]
+                    b_row = coords[b][0]
+                    b_col = coords[b][1]
+
+                    if a_row < b_row:
+                        row_space = sum([a_row <= z < b_row for z in empty_rows])
+                    else:
+                        row_space = sum([b_row <= z < a_row for z in empty_rows])
+                    if a_col < b_col:
+                        col_space = sum([a_col <= z < b_col for z in empty_cols])
+                    else:
+                        col_space = sum([b_col <= z < a_col for z in empty_cols])
+
+                    delta = abs(b_row - a_row) + col_space*999999 + abs(b_col - a_col) + row_space*999999
+                    total += delta
+                    pass
         print(total)
 
 
